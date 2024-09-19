fe3366e1d0dcf25e37f789e0cf1e0aae5435013e
Nasirudeen Olohundare <iamnasirudeen@gmail.com>
2024-09-19 02:53:07 +0100

    fixes

--- bin/sls.js
+++ bin/sls.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
-
-const path = require("path");
-const fs = require("fs");
 const pkg = require("../package.json");
-const { scanDirectory } = require("../lib/fileScanner");
-const { scanGitCommitsForSecrets } = require("../lib/gitScanner");
 const { program } = require("commander");
-const { configHandler } = require("../lib/configHandler");
-const { buildCustomDetectors } = require("../lib/regexPatterns");
-const { analyzeRepository } = require("../lib/urlScanner");
+const chalk = require("chalk");
+const { scan } = require("../lib/scan");
+const { analyzers } = require("../lib/analyzers");
+
+program.name(pkg.name).description(pkg.description).version(pkg.version);
 
 program
-  .name(pkg.name)
-  .description(pkg.description)
-  .version(pkg.version)
+  .command("scan")
+  .description("Scan an entire codebase for secrets")
   .option(
     "-e, --exclude <folders>",
     "Comma-separated list of folders to exclude from scanning",
@@ -25,90 +21,37 @@ program
   .option("-d, --changed", "Only scan changed files and lines", false)
   .option(", --url <url>", "A link to a Github, Gitlab or BitBucket URL", "")
   .option(
-    "-v, --verify <boolean>",
+    "-v, --verify",
     "Should be specified if secrets should be verified",
     false
   )
-  .parse(process.argv);
+  .option("-m, --mask", "Should mask secret values", false)
+  .action(async (options) => await scan(options));
 
-const options = program.opts();
-const startDirectory = options.dir || process.cwd();
-let excludedFolders = options.exclude
-  .split(",")
-  .map((folder) => folder.trim())
-  .filter((folder) => folder);
-const commitLimit = parseInt(options.commits, 10);
-const configFile = options.config;
-
-const main = async () => {
-  try {
-    /**
-     * check if user has a config file and merge it with the existing configs
-     */
-    const config = configHandler(configFile);
-    if (config && config.exclude && config.exclude.paths)
-      excludedFolders = [
-        ...new Set([excludedFolders || [], config.exclude.paths].flat()),
-      ];
-
-    /**
-     * merge default regex alongside users custom regex
-     */
-    const userConfigRexes = (config && config.detectors) || {};
-    const customDetectors = buildCustomDetectors(userConfigRexes);
-
-    const excludedExtensions =
-      (config && config.exclude && config.exclude.extensions) || [];
-
-    /**
-     * if user is trying to scan URL, --changed flag is not respected anymore
-     */
-    if (options.url) {
-      return await analyzeRepository({
-        url: options.url,
-        excludedFolders,
-        excludedExtensions,
-        verify: options.verify,
-      });
+program
+  .command("analyze")
+  .option("-s, --secret <string>", "Secret to analyze")
+  .argument("<service>", "service to analyze")
+  .description("Analyze secrets")
+  .requiredOption(
+    "-s, --secret <string>",
+    "secret to analyze must be specified"
+  )
+  .action(async (service, options) => {
+    const serviceExist = analyzers.find(
+      (analyzer) => service.toLowerCase() === Object.keys(analyzer)[0]
+    );
+    if (!serviceExist) {
+      console.error(
+        chalk.red(
+          `[x] Error: ${chalk.greenBright(
+            "github, gitlab, mongodb, mysql, postgresql, slack"
+          )} are the only supported analyzers for now`
+        )
+      );
+      process.exit(1);
     }
+    await serviceExist[service.toLowerCase()](options.secret);
+  });
 
-    /**
-     * only scan all directories if user did not specify to scan only changed file
-     */
-    if (!options.changed) {
-      // Scan the codebase
-      await scanDirectory({
-        startDirectory,
-        excludedFolders,
-        excludedExtensions,
-        verify: options.verify,
-        customDetectors,
-      });
-    }
-
-    // Scan .git commits if present
-    const gitDir = path.join(startDirectory, ".git");
-    if (fs.existsSync(gitDir)) {
-      await scanGitCommitsForSecrets({
-        startDirectory,
-        commitLimit,
-        changed: options.changed,
-        excludedFolders,
-        verify: options.verify,
-      });
-    } else {
-      /**
-       * I assume its normal for users not to have git repo in their project so
-       * I only log a message if user specified `--changed` flag which is strict on
-       * only git scanning
-       */
-      if (options.changed) {
-        console.log("Repository doesnt have a .git directory");
-      }
-    }
-  } catch (error) {
-    console.error(`Error scanning: ${error.message}`);
-  }
-};
-
-main();
+program.parse(process.argv);
