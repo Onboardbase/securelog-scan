fd3bdf7bde2597d064e03ec1ca0342b710ea8713
Nasirudeen Olohundare <iamnasirudeen@gmail.com>
2024-12-19 18:32:55 +0100

    feat: 3.0.17

---
+++ src/detectors/sentry/index.ts
@@ -0,0 +1,51 @@
+import Re2 from "re2";
+import { Detector, ScanResult } from "../../types/detector";
+import { surroundWithGroups } from "../../regexHandler";
+import { httpClient } from "../../util";
+
+const keywords: string[] = ["sentry"];
+const keyPattern: Re2 = new Re2(
+  `${surroundWithGroups(keywords)}\\b([a-f0-9]{64})\\b`,
+  "gi"
+);
+
+const scan = async (
+  verify: boolean | undefined,
+  data: string
+): Promise<ScanResult | null> => {
+  const keyPatternMatches = data.matchAll(keyPattern);
+
+  const result: ScanResult = { detectorType: "Sentry", verified: false };
+
+  for (const match of keyPatternMatches) {
+    if (match.length !== 2) continue;
+
+    const resMatch: string = match[1].trim();
+    result.rawValue = resMatch;
+    result.position = match.index;
+
+    if (verify) {
+      try {
+        await httpClient.get("https://api.vercel.com/www/user", {
+          headers: {
+            Authorization: `Bearer ${resMatch}`,
+          },
+        });
+
+        result.verified = true;
+      } catch (error) {}
+    }
+
+    return result;
+  }
+
+  return null;
+};
+
+const detectorType = "SENTRY_DETECTOR";
+
+export const SentryDetector: Detector = {
+  scan,
+  keywords,
+  detectorType,
+};
---
+++ src/detectors/shopify/index.ts
@@ -0,0 +1,53 @@
+import Re2 from "re2";
+import { Detector, ScanResult } from "../../types/detector";
+import { httpClient } from "../../util";
+
+const keywords: string[] = ["shppa_", "shpat_"];
+const keyPattern: Re2 = new Re2("\\b(shppa_|shpat_)([0-9A-Fa-f]{32})\\b", "gi");
+const domainPattern = new Re2(/^[a-zA-Z0-9-]+\.myshopify\.com$/, "gi");
+
+const scan = async (
+  verify: boolean | undefined,
+  data: string
+): Promise<ScanResult | null> => {
+  const keyPatternMatches = data.matchAll(keyPattern);
+  const domainPatternMatches = data.matchAll(domainPattern);
+  let result: ScanResult = { detectorType: "Shopify", verified: false };
+
+  for (const match of keyPatternMatches) {
+    const shopifyKey = match?.[0]?.trim();
+
+    result.rawValue = shopifyKey;
+    result.position = match.index;
+
+    for (const domainMatch of domainPatternMatches) {
+      const domainMatchValue = domainMatch?.[0]?.trim();
+
+      if (verify) {
+        try {
+          await httpClient.get(
+            `https://${domainMatchValue}/admin/oauth/access_scopes.json`,
+            {
+              headers: {
+                "X-Shopify-Access-Token": shopifyKey,
+              },
+            }
+          );
+
+          result.verified = true;
+        } catch (error) {}
+      }
+      return result;
+    }
+  }
+
+  return null;
+};
+
+const detectorType = "SHOPIFY_DETECTOR";
+
+export const ShopifyDetector: Detector = {
+  scan,
+  keywords,
+  detectorType,
+};
